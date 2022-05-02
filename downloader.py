6dd6e81abdc03132cb8b1056fb4f74ca4327c52a
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 17:13:40 -0600

    Mon May  2 23:13:40 2022|/Users/jarndt/code_projects/leetcode/downloader.py|PlatformSpecific Updated IsFile

--- downloader.py
+++ downloader.py
@@ -55,7 +55,7 @@ class Example:
                                                                                              self.explanation))
 
 
-base_url = 'https://leetcode.com/problems/two-sum/'
+base_url = 'https://leetcode.com/problems/add-two-numbers/'
 # make directory for new problem
 dir_name = base_url.split('/')
 if dir_name[-1].replace('/', ''):
