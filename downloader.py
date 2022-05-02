ca5ceb4ac09703e7a1198000692b9b124a5cd6ae
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 11:17:31 -0600

    Initial commit

--- downloader.py
+++ downloader.py
@@ -4,8 +4,14 @@ import requests
 from bs4 import BeautifulSoup
 
 base_url = 'https://leetcode.com/problems/two-sum/'
+# make directory for new problem
+dir_name = base_url.split('/')
+if base_url[-1].replace('/',''):
+    dir_name = base_url[-1]
+else:
+    dir_name = base_url[-2]
+os.mkdir(dir_name)
+
+
 soup = BeautifulSoup(requests.get(base_url).content, "html.parser")
 print(soup)
-# key = soup.select_one("#ncaptchaRecaptchaId")["data-sitekey"]
-# make directory for new problem
-os.mkdir()
