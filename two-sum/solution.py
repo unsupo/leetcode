4dedfb2a307c9185d4765b09529e3c072ad51e36
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 17:10:26 -0600

    Mon May  2 23:10:26 2022|/Users/jarndt/code_projects/leetcode/two-sum/solution.py|PlatformSpecific Updated IsFile

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -5,22 +5,26 @@ class Solution(object):
         :type target: 9
         :rtype: [0,1]
         """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         # Have solution here and remove pass
         pass
 
-        
+
 def test0():
-    assert Solution()._Two_Sum([2,7,11,15], 9) == [0,1]
+    assert Solution()._Two_Sum([2, 7, 11, 15], 9) == [0, 1]
 
-            
+
 def test1():
-    assert Solution()._Two_Sum([3,2,4], 6) == [1,2]
+    assert Solution()._Two_Sum([3, 2, 4], 6) == [1, 2]
 
-            
+
 def test2():
-    assert Solution()._Two_Sum([3,3], 6) == [0,1]
+    assert Solution()._Two_Sum([3, 3], 6) == [0, 1]
 
-            
+
 if __name__ == '__main__':
     test0()
     test1()
