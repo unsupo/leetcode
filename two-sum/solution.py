3003d5e3812f7ea91c7e0959d8ac537710654186
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 16:11:03 -0600

    Mon May  2 22:11:03 2022|/Users/jarndt/code_projects/leetcode/two-sum/solution.py|Updated IsFile

---
+++ two-sum/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def _Two_Sum(self, nums, target):
+        """
+        :type nums: [3,3]
+        :type target: 6
+        :rtype: [0,1]
+        """
+        # Have solution here and remove pass
+        pass
+
+        
