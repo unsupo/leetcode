fa3472562d19b4d350fc72a4ac08c6a262a48aae
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 12:44:48 -0600

    Mon

--- downloader.py
+++ downloader.py
@@ -4,7 +4,12 @@ import markdownify
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 base_url = 'https://leetcode.com/problems/two-sum/'
 # make directory for new problem
@@ -20,7 +25,11 @@ except FileExistsError:
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(base_url)
-
+delay=3
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-cy="question-title"]')))
+except TimeoutException as e:
+    raise e
 soup = BeautifulSoup(requests.get(base_url).content, "html.parser")
 # title=
 # description=
