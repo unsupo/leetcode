6d0ce260931076ef1f576d736e389bb9763814ed
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 14:17:42 -0600

    Mon May  2 20:17:42 2022|/Users/jarndt/code_projects/leetcode/downloader.py|PlatformSpecific Updated IsFile

--- downloader.py
+++ downloader.py
@@ -37,41 +37,41 @@ soup = BeautifulSoup(driver.page_source, "html.parser")
 driver.close()
 title = soup.select('div[data-cy="question-title"]')[0].contents[0]
 metadata = soup.select('div[data-cy="question-title"]')[0].parent.contents[1].contents
-diff=metadata[0].text
-up_votes=metadata[1].text
-down_votes=metadata[2].text
-description_data=soup.select('div[data-cy="question-title"]')[0].parent.parent.contents[1].contents[0].contents
-description=""
-examples=[]
-example=""
-constraints=""
-follow_up=""
+diff = metadata[0].text
+up_votes = metadata[1].text
+down_votes = metadata[2].text
+description_data = soup.select('div[data-cy="question-title"]')[0].parent.parent.contents[1].contents[0].contents
+description = ""
+examples = []
+example = ""
+constraints = ""
+follow_up = ""
 desc = True
 exam = False
 cons = False
 foll = False
 for content in description_data:
     if '<p><strong>Example' in str(content):
-        desc=False
-        exam=True
+        desc = False
+        exam = True
         if example:
             examples.append(example)
-            example=""
+            example = ""
     if '<p><strong>Constraints:</strong></p>' == str(content.text):
-        cons=True
+        cons = True
     if '<strong>Follow-up: </strong>' == str(content.text):
-        foll=True
-    if desc: description+=markdownify.markdownify(str(content), heading_style="ATX")
-    if exam: example+=markdownify.markdownify(str(content), heading_style="ATX")
-    if cons: constraints+=markdownify.markdownify(str(content), heading_style="ATX")
-    if foll: follow_up+=markdownify.markdownify(str(content), heading_style="ATX")
+        foll = True
+    if desc: description += markdownify.markdownify(str(content), heading_style="ATX")
+    if exam: example += markdownify.markdownify(str(content), heading_style="ATX")
+    if cons: constraints += markdownify.markdownify(str(content), heading_style="ATX")
+    if foll: follow_up += markdownify.markdownify(str(content), heading_style="ATX")
 
 # dumb way to get all data, doesn't work when trying to templatize tests and such
 # main = soup.select('div[data-cy="question-detail-main-tabs"]')[0]
 # h = markdownify.markdownify(str(main), heading_style="ATX")
 
 os.remove(dir_name + '/README.md')
-with open(dir_name + '/README.md','w') as f:
-    f.write('*{}*\n{} {} {}\n---\n{}'.replace('\n','\n</br>').format(title,diff,up_votes,down_votes,description))
+with open(dir_name + '/README.md', 'w') as f:
+    f.write('# {}\n<br/>{} {} {}<br/>\n---<br/>\n{}'.format(title, diff, up_votes, down_votes, description))
 
 print(example)
