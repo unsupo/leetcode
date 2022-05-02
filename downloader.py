b943383c00a9fda7b74b07969b504317690fe026
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 11:12:57 -0600

    Initial commit

--- downloader.py
+++ downloader.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,3 +8,5 @@ problem = 'two-sum/'
 soup = BeautifulSoup(requests.get(base_url+problem).content, "html.parser")
 print(soup)
 # key = soup.select_one("#ncaptchaRecaptchaId")["data-sitekey"]
+# make directory for new problem
+os.mkdir()
