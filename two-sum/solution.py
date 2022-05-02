103d27a0bee9ae6e2e76a4b7e390811a2c8d58a8
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 15:56:59 -0600

    Mon May  2 21:56:59 2022|/Users/jarndt/code_projects/leetcode/two-sum/solution.py|Updated IsFile

---
+++ two-sum/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def _Two_Sum(self, nums, target):
+        """
+        :type nums: [3,3]
+        :type target: 6
+        :rtype: [0,1]
+        """
+        # Have solution here
+        pass
+
+        def test0():
+    assert Solution()._Two_Sum([3,3], 6) == [0,1]
+
+            def test1():
+    assert Solution()._Two_Sum([3,3], 6) == [1,2]
+
+            def test2():
+    assert Solution()._Two_Sum([3,3], 6) == [0,1]
+
+            
