18fc7724c71db2454b15fb32053fa3e8c9050b03
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 16:38:51 -0600

    Mon May  2 22:38:51 2022|/Users/jarndt/code_projects/leetcode/two-sum/README.md|Removed IsFile

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -2,7 +2,7 @@ class Solution(object):
     def _Two_Sum(self, nums, target):
         """
         :type nums: [3,3]
-        :type target: 
+        :type target: 6
         :rtype: [0,1]
         """
         # Have solution here and remove pass
@@ -10,15 +10,15 @@ class Solution(object):
 
         
 def test0():
-    assert Solution()._Two_Sum([3,3], ) == [0,1]
+    assert Solution()._Two_Sum([3,3], 6) == [0,1]
 
             
 def test1():
-    assert Solution()._Two_Sum([3,3], ) == [1,2]
+    assert Solution()._Two_Sum([3,3], 6) == [1,2]
 
             
 def test2():
-    assert Solution()._Two_Sum([3,3], ) == [0,1]
+    assert Solution()._Two_Sum([3,3], 6) == [0,1]
 
             
 if __name__ == '__main__':
