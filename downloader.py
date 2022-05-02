680d516cd6d06993aa9964c01840d22ff701aaf4
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 11:18:12 -0600

    Initial commit

--- downloader.py
+++ downloader.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 base_url = 'https://leetcode.com/problems/two-sum/'
 # make directory for new problem
 dir_name = base_url.split('/')
-if base_url[-1].replace('/',''):
-    dir_name = base_url[-1]
+if dir_name[-1].replace('/',''):
+    dir_name = dir_name[-1]
 else:
-    dir_name = base_url[-2]
+    dir_name = dir_name[-2]
 os.mkdir(dir_name)
 
 
