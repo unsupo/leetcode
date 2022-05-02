96a4ab668690adcba591ad99704e55a1a2983b20
Jonathan Arndt <jarndt@salesforce.com>
2022-05-02 11:13:30 -0600

    Mon May  2 16:37:40 2022|/Users/jarndt/code_projects/leetcode/venv/lib/python3.9/site-packages/setuptools/command/__pycache__/__init__.cpython-39.pyc.4419733968|Created Renamed Updated IsFile

--- downloader.py
+++ downloader.py
@@ -3,9 +3,8 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://leetcode.com/problems/'
-problem = 'two-sum/'
-soup = BeautifulSoup(requests.get(base_url+problem).content, "html.parser")
+base_url = 'https://leetcode.com/problems/two-sum/'
+soup = BeautifulSoup(requests.get(base_url).content, "html.parser")
 print(soup)
 # key = soup.select_one("#ncaptchaRecaptchaId")["data-sitekey"]
 # make directory for new problem
