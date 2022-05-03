f49fe720145391e934710c1b148a67d9c2dbab55
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 10:15:35 -0600

    Tue May  3 16:15:35 2022|/Users/jarndt/code_projects/leetcode/add-two-numbers/solution.py|PlatformSpecific Updated IsFile

--- add-two-numbers/solution.py
+++ add-two-numbers/solution.py
@@ -45,10 +45,12 @@ class Solution(object):
         return l5
 
     def _Add_Two_Numbers(self, l1, l2):
-        l3=l1
-        l4=ListNode()
+        l3 = l1
+        l4 = None
         while l3.next:
-            v = l1[i] + l2[i]
+            v = l1.val + l2.val
+            l1 = l1.next
+            l2 = l2.next
             if r > 0:
                 v += r
                 if v < 10:
@@ -56,22 +58,26 @@ class Solution(object):
             if v // 10 >= 1:
                 r = v // 10
                 v = v % 10
-            l5.append(v)
+            l4 = ListNode(v, l4)
             if not l3.next: l3 = l2
         if r > 0:
-            l5.append(r)
-
+            l4 = ListNode(r, l4)
+        return l4
 
 def test0():
-    assert Solution()._Add_Two_Numbers(ListNode.to_list_node([2, 4, 3]), ListNode.to_list_node([5, 6, 4])) == ListNode.to_list_node([7, 0, 8])
+    assert Solution()._Add_Two_Numbers(ListNode.to_list_node([2, 4, 3]),
+                                       ListNode.to_list_node([5, 6, 4])) == ListNode.to_list_node([7, 0, 8])
 
 
 def test1():
-    assert Solution()._Add_Two_Numbers(ListNode.to_list_node([0]), ListNode.to_list_node([0])) == ListNode.to_list_node([0])
+    assert Solution()._Add_Two_Numbers(ListNode.to_list_node([0]), ListNode.to_list_node([0])) == ListNode.to_list_node(
+        [0])
 
 
 def test2():
-    assert Solution()._Add_Two_Numbers(ListNode.to_list_node([9, 9, 9, 9, 9, 9, 9]), ListNode.to_list_node([9, 9, 9, 9])) == ListNode.to_list_node([8, 9, 9, 9, 0, 0, 0, 1])
+    assert Solution()._Add_Two_Numbers(ListNode.to_list_node([9, 9, 9, 9, 9, 9, 9]),
+                                       ListNode.to_list_node([9, 9, 9, 9])) == ListNode.to_list_node(
+        [8, 9, 9, 9, 0, 0, 0, 1])
 
 
 if __name__ == '__main__':
