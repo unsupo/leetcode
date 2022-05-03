ddbe7891594a41bdf7690b0d9385dc8b467718ef
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 17:03:43 -0600

    Tue May  3 23:03:43 2022|/Users/jarndt/code_projects/leetcode/longest-palindromic-substring/solution.py|PlatformSpecific Updated IsFile

--- longest-palindromic-substring/solution.py
+++ longest-palindromic-substring/solution.py
@@ -6,9 +6,23 @@ from time import time
 
 class Solution(object):
     memoize = {}
-    def _Longest_Palindromic_Substring(self, s):
-        
 
+    def _Longest_Palindromic_Substring(self, s):
+        def helper(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l + 1:r]
+
+        res = ""
+        for i in range(len(s)):
+            tmp = helper(i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            tmp = helper(i, i + 1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
 
     def attempt3(self, s):
         if s == s[::-1]: return s
