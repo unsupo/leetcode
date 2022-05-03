3da366f790f816cc4e0230401ac693d9346b0b34
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 09:36:19 -0600

    Tue May  3 15:36:19 2022|/Users/jarndt/code_projects/leetcode/add-two-numbers/solution.py|PlatformSpecific Updated IsFile

--- add-two-numbers/solution.py
+++ add-two-numbers/solution.py
@@ -10,7 +10,7 @@ class Solution(object):
         l5 = []
         for i in range(len(l1)):
             v = l1[i]+l2[2]
-            
+
         
 def test0():
     assert Solution()._Add_Two_Numbers([2,4,3], [5,6,4]) == [7,0,8]
