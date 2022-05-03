50e9f892f34673f6adbfa44554db9d1e6f2aaa35
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 14:13:51 -0600

    Tue May  3 20:13:51 2022|/Users/jarndt/code_projects/leetcode/longest-palindromic-substring/solution.py|PlatformSpecific Updated IsFile

--- longest-palindromic-substring/solution.py
+++ longest-palindromic-substring/solution.py
@@ -9,11 +9,11 @@ class Solution(object):
         l = len(s)
         for i in range(1, l):
             if s[i:l] == s[i:l][::-1]:
-                return s[i:l]  
+                return s[i:l]
             if s[:-i] == s[:-i][::-1]:
                 return s[:-i]
-            if s[:-i] == s[:-i][::-1]:
-                return s[:-i]
+            if s[i:-i] == s[i:-i][::-1]:
+                return s[i:-i]
         return None
 
 
