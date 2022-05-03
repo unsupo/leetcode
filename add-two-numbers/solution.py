e49d1ddd08055d8d575a002792cbc5dc0cca2e2e
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 09:45:01 -0600

    Tue May  3 15:45:01 2022|/Users/jarndt/code_projects/leetcode/add-two-numbers/solution.py|PlatformSpecific Updated IsFile

--- add-two-numbers/solution.py
+++ add-two-numbers/solution.py
@@ -5,32 +5,32 @@ class Solution(object):
         :type l2: [5,6,4]
         :rtype: [7,0,8]
         """
-        # l3 = l1[::-1]
-        # l4 = l2[::-1]
         l5 = []
         r = 0
         for i in range(len(l1)):
-            v = l1[i]+l2[i]
+            v = l1[i] + l2[i]
             if r > 0:
                 v += r
                 r = 0
-            if v//10 >= 1:
-                r = v//10
-                v = v%10
+            if v // 10 >= 1:
+                r = v // 10
+                v = v % 10
             l5.append(v)
+        return l5
+
 
 def test0():
-    assert Solution()._Add_Two_Numbers([2,4,3], [5,6,4]) == [7,0,8]
+    assert Solution()._Add_Two_Numbers([2, 4, 3], [5, 6, 4]) == [7, 0, 8]
 
-            
+
 def test1():
     assert Solution()._Add_Two_Numbers([0], [0]) == [0]
 
-            
-def test2():
-    assert Solution()._Add_Two_Numbers([9,9,9,9,9,9,9], [9,9,9,9]) == [8,9,9,9,0,0,0,1]
 
-            
+def test2():
+    assert Solution()._Add_Two_Numbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]) == [8, 9, 9, 9, 0, 0, 0, 1]
+
+
 if __name__ == '__main__':
     test0()
     test1()
