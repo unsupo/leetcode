d14d52bf615b9cca62772c593142add2bd93462e
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 13:39:06 -0600

    Tue May  3 19:39:06 2022|/Users/jarndt/code_projects/leetcode/median-of-two-sorted-arrays/solution.py|PlatformSpecific Updated IsFile

--- median-of-two-sorted-arrays/solution.py
+++ median-of-two-sorted-arrays/solution.py
@@ -6,17 +6,18 @@ class Solution(object):
         :rtype: 2.00000
         """
         nums3 = nums1.extend(nums2)
-        return nums3[len(nums3//2)] if len(nums3)%2==1 else (nums3[len(nums3//2)]+nums3[len(nums3//2)-1])/2
+        return nums3[len(nums3 // 2)] if len(nums3) % 2 == 1 else \
+            (nums3[len(nums3 // 2)] + nums3[len(nums3 // 2) - 1]) / 2
 
-        
+
 def test0():
-    assert Solution()._Median_of_Two_Sorted_Arrays([1,3], [2]) == 2.00000
+    assert Solution()._Median_of_Two_Sorted_Arrays([1, 3], [2]) == 2.00000
 
-            
+
 def test1():
-    assert Solution()._Median_of_Two_Sorted_Arrays([1,2], [3,4]) == 2.50000
+    assert Solution()._Median_of_Two_Sorted_Arrays([1, 2], [3, 4]) == 2.50000
 
-            
+
 if __name__ == '__main__':
     test0()
     test1()
