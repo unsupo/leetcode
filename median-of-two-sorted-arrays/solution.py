8dca5d5216b607d97f56f7b8b2bebd26376f7c18
Jonathan Arndt <jarndt@salesforce.com>
2022-05-03 13:38:08 -0600

    Tue May  3 19:38:08 2022|/Users/jarndt/code_projects/leetcode/median-of-two-sorted-arrays/solution.py|PlatformSpecific Updated IsFile

--- median-of-two-sorted-arrays/solution.py
+++ median-of-two-sorted-arrays/solution.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: 2.00000
         """
         nums3 = nums1.extend(nums2)
-        return nums3[len(nums3)] if len(nums3)%2==1
+        return nums3[len(nums3//2)] if len(nums3)%2==1 else
 
         
 def test0():
