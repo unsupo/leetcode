4bf544a2400e3113a983165a0a9643b9b0686380
Jonathan Arndt <jarndt@salesforce.com>
2022-05-04 17:16:59 -0600

    Wed May  4 23:16:59 2022|/Users/jarndt/code_projects/leetcode/regular-expression-matching/solution.py|PlatformSpecific Updated IsFile

--- regular-expression-matching/solution.py
+++ regular-expression-matching/solution.py
@@ -9,13 +9,16 @@ class Solution(object):
         if not p or p[0] == "*": return False
         # attempt to split by * call these groups
         groups = []
-        g = "" # i'll assume i can't split here since i can't use re
-        for i in range(len(p)):
+        g = ""  # i'll assume i can't split here since i can't use re
+        i = 0
+        while i < len(p):
             if p[i] == "*":
                 groups.append(g)
-                groups.append(p[i-1:i+1])
+                groups.append(p[i - 1:i + 1])
                 g = ""
+                i += 1
             g += p[i]
+            i += 1
         # now i have groups like asdf, a*, b*, .* ect
         # order matters, find the first group if not then return false
         # if true move to the next group
@@ -64,6 +67,7 @@ def test2():
 
 def test3():
     assert Solution()._Regular_Expression_Matching("rrraaaa", "r*a*") == True
+
 
 def test4():
     assert Solution()._Regular_Expression_Matching("drrraomnklbbbbc", "dr*a.*b*c") == True
