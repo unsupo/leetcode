52e33d9ae07725cf1d4f83d918cb15859ca7a226
Jonathan Arndt <jarndt@salesforce.com>
2022-05-04 14:32:35 -0600

    Wed May  4 20:32:35 2022|/Users/jarndt/code_projects/leetcode/string-to-integer-atoi/solution.py|PlatformSpecific Updated IsFile

--- string-to-integer-atoi/solution.py
+++ string-to-integer-atoi/solution.py
@@ -4,33 +4,38 @@ class Solution(object):
         :type s: "42"
         :rtype: 42
         """
-        num=""
-        start=False
+        num = ""
+        start = False
         for i in range(len(s)):
             if s[i] in ('-', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
                 start = True
             elif start:
                 break
-            if start: num+=s[i]
+            if start: num += s[i]
         r = int(num)
-        if r > 2**31-1: return 2**31-1
-        if r < -2**31: return -2**31
+        if r > 2 ** 31 - 1: return 2 ** 31 - 1
+        if r < -2 ** 31: return -2 ** 31
         return r
 
-        
+
 def test0():
     assert Solution()._String_to_Integer_atoi("42") == 42
 
-            
+
 def test1():
     assert Solution()._String_to_Integer_atoi("   -42") == -42
 
-            
+
 def test2():
     assert Solution()._String_to_Integer_atoi("4193 with words") == 4193
 
-            
+
+def test3():
+    assert Solution()._String_to_Integer_atoi("4193 with words") == 4193
+
+
 if __name__ == '__main__':
     test0()
     test1()
     test2()
+    test3()
