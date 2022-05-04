e95b3f9fd67540db3d2a1c52c616aa17f8f8ad9d
Jonathan Arndt <jarndt@salesforce.com>
2022-05-04 15:56:02 -0600

    Wed May  4 21:56:02 2022|/Users/jarndt/code_projects/leetcode/regular-expression-matching/solution.py|PlatformSpecific Updated IsFile

--- regular-expression-matching/solution.py
+++ regular-expression-matching/solution.py
@@ -9,24 +9,26 @@ class Solution(object):
         expand = False
         expand_char = ""
         for i in range(len(s)):
-            expand_char = s[i]
-            
+            if s[i] not in ('.', '*'):
+                expand_char = s[i]
+                expand = False
+            else:
+                expand = True
             s[i]
 
 
-        
 def test0():
     assert Solution()._Regular_Expression_Matching("aa", "a") == false
 
-            
+
 def test1():
     assert Solution()._Regular_Expression_Matching("aa", "a\*") == true
 
-            
+
 def test2():
     assert Solution()._Regular_Expression_Matching("ab", ".\*") == true
 
-            
+
 if __name__ == '__main__':
     test0()
     test1()
