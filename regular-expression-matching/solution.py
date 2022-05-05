f28e4a7a251851dba45d9589b98fbf9f7303149b
Jonathan Arndt <jarndt@salesforce.com>
2022-05-05 12:25:09 -0600

    Thu May  5 18:25:08 2022|/Users/jarndt/code_projects/leetcode/regular-expression-matching/solution.py|PlatformSpecific Updated IsFile

--- regular-expression-matching/solution.py
+++ regular-expression-matching/solution.py
@@ -39,6 +39,9 @@ class Solution(object):
                 if expand:
                     if expand_char == '.' or s[str_index] == expand_char:
                         # if it's a dot then check the next non group to see if it matches
+                        if expand_char == '.':
+                            nxt_group_index = 0
+                            
                         found = True  # if it matches then stay on this group
                     else:
                         str_index -= 1
